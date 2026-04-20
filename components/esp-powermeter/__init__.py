5a73ec56bb36d08abbbb981032e4e73f0b855660
Matthias Neubauer <m_neubauer@gmx.de>
2026-04-20 21:58:04 +0200

    Update __init__.py

--- components/esp-powermeter/__init__.py
+++ components/esp-powermeter/__init__.py
@@ -2,8 +2,13 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
+AUTO_LOAD = ["binary_sensor", "sensor", "text_sensor"]
+
+DEPENDENCIES = ["uart"]
+
+
 esp-powermeter_ns = cg.esphome_ns.namespace("esp-powermeter")
-EspPowermeterComponent = esp-powermeter_ns.class_("EspPowermeterComponent", cg.Component)
+EspPowermeterComponent = esp-powermeter_ns.class_("EspPowermeterComponent", uart.UARTDevice, cg.Component)
 
 CONFIG_SCHEMA = cv.Schema(
     {
